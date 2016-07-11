0a337f33ce4adfd67471331665cd6e60030e2c3c
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-07-10 21:38:07 -0500

    add initial api for orders

---
+++ app/models/order.rb
@@ -0,0 +1,3 @@
+class Order < ActiveRecord::Base
+    validates :product_name, :amount_in_cents, :order_date, presence:true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+500.times do
+  Order.create!  product_name: Faker::Company.catch_phrase,
+                amount_in_cents:(500..50000).to_a.sample,
+                order_date: (1..1500).to_a.sample.days.ago
+end
