dd22a2f2cb17c2a1e562d6e4beed08dd7747e97b
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-07-10 22:54:49 -0500

    crud for orders

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -9,4 +9,29 @@ before_action :doorkeeper_authorize!
   def show
     @order = Order.find_by id: params[:id]
   end
+
+  def delete
+    @order = Order.find_by id: params[:id]
+    @order.destroy
+    head :ok
+  end
+
+  def create
+    @order = Order.new params.require(:order).permit(:product_name, :order_date)
+    if @order.save
+      render :show
+    else
+      render json: @order.errors, status: 422
+    end
+  end
+
+  def update
+    @order = Order.find_by id: params[:id]
+    if @order.update params.require(:order).permit(:product_name, :order_date)
+      render :show
+    else
+      render json: @order.errors, status: 422
+    end
+  end
+
 end
